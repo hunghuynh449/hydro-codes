3c9efeca60b36fdb25a3253fe92de291cedad676
Huỳnh Quốc Hưng <49772925+hunghuynh449@users.noreply.github.com>
2024-02-20 11:50:29 +0700

    add google tag

--- container/Header/index.tsx
+++ container/Header/index.tsx
@@ -9,11 +9,21 @@ const Header: React.FC = () => {
       <link rel="icon" href="/heart.ico" />
 
       <meta itemProp="name" content="Website code linh tinh của Hydro" />
-          <meta itemProp="description" content="Thư viện link project" />
-          <meta
-            itemProp="image"
-            content="http://www.pngkit.com/png/detail/186-1867486_pandablindfold-discord-emoji-admiral-bahroo-emotes.png"
-          />
+      <meta itemProp="description" content="Thư viện link project" />
+      <meta
+        itemProp="image"
+        content="http://www.pngkit.com/png/detail/186-1867486_pandablindfold-discord-emoji-admiral-bahroo-emotes.png"
+      />
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-87LJ87TDHV"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-87LJ87TDHV');
+      </script>
     </Head>
   );
 };
